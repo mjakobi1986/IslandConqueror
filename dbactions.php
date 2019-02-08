d1f87741e093780049b6b54c9d8f9fc61bf21464
mjakobi1986 <matjak6891@yahoo.de>
2019-02-08 15:51:31 +0100

    Update dbactions.php

--- dbactions.php
+++ dbactions.php
@@ -4,14 +4,21 @@
 	$user = "root";
 	$password = "";
 
-	$conn = new mysqli_connect('p:host=localhost:3306;dbname=eroberer_noenemies_nostorage', $user, $password);
+	$conn = new mysqli('p:host=localhost:3306;dbname=eroberer_noenemies_nostorage', $user, $password);
 	if (!$conn) {
 	   $m = oci_error();
 	   echo $m['message'], "\n";
 	   exit;
 	}
-	else {
-	   print "Connected";
+
+	function parseSQLUpdateString ($tableName, $column, $newValue, $searchedCol, $searchedValue) {
+		$sqlString = $conn -> prepare("UPDATE ? SET ? = ? WHERE ? = ?");
+		$sqlString -> bind_param('ssiss', $tablename, $column, $newValue, $searchedCol, $searchedValue);
+		$sqlString = $conn -> real_escape_string($sqlString);
+		
+		return $sqlString;
 	}
+
+	function parseSQLSelectString ()
 
  ?>
